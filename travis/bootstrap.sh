2e867864907c73fcdd01eb3c2c9ddd819b6ac067
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-10-27 22:55:57 -0700

    travis: make tests pass with latest almond-server

--- travis/bootstrap.sh
+++ travis/bootstrap.sh
@@ -4,6 +4,7 @@ mkdir ~/almond-server
 echo '{"server-login":{"password":"x","salt":"x","sqliteKeySalt":"x"}}' > ~/almond-server/prefs.db
 docker run -p 3000:3000 \
   -v ~/almond-server:/var/lib/almond-server \
+  -e THINGENGINE_HOST_BASED_AUTHENTICATION=insecure \
   stanfordoval/almond-server:latest-portable &
 
 sleep 60
